80ac9a28ae67032ab0076353bfbdec479594d5a9
Ankit Srivastava <42239246+ankitsri98@users.noreply.github.com>
2019-11-21 03:59:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
 
+const express=require('express');
+const connectdb=require('./db/connection');
+const app=express();
+connectdb();
+
+app.use(express.json({extended:false}));
+app.use('/api/usermodel',require('./api/user'));
+
+const Port=process.env.Port||3000;
+
+app.listen(Port,()=>console.log('server started'));
